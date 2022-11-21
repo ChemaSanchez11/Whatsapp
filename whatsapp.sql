21af0155d505f11d47875d3ce3b81bf7da239bc4
Areku92 <jusnjimenezmolna1@gmail.com>
2022-11-21 20:39:50 +0100

    cambiamos base de datos

--- whatsapp.sql
+++ whatsapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2022 a las 19:39:43
+-- Tiempo de generación: 21-11-2022 a las 20:38:31
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
@@ -39,6 +39,29 @@ CREATE TABLE `chat` (
 INSERT INTO `chat` (`id`, `text`) VALUES
 (1, 'Este es el primer comentario');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` int(11) NOT NULL DEFAULT 0,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `firstname`, `lastname`, `password`, `rol`, `img`) VALUES
+(3, 'Chema', '', NULL, 'c4ca4238a0b923820dcc509a6f75849b', 0, 'default.png');
+
 --
 -- Índices para tablas volcadas
 --
@@ -50,6 +73,12 @@ ALTER TABLE `chat`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -58,6 +87,12 @@ ALTER TABLE `chat`
 --
 ALTER TABLE `chat`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
