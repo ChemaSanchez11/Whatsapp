9329f9ba2e914d8d7678e4ed02ad1d966e0a9a2c
ChemaSanchez <josemaria.chauchina@gmail.com>
2022-11-21 18:26:46 +0100

    Cambio en SQL

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@
 class Database
 {
     public static function conexion(){
-        $conexion=new mysqli("localhost", "root", "", "carrito");
+        $conexion=new mysqli("localhost", "root", "", "whatsapp");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,9 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXIST `whatsapp`;
+USE `whatsapp`;
+
     -- ----------------------------
 -- Table structure for users
 -- ----------------------------
