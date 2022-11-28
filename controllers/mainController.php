7be3364c9887f5860ab8e0b69c13b3939b4e35ed
ChemaSanchez <josemaria.chauchina@gmail.com>
2022-11-28 18:00:20 +0100

    Cambios 24/11/2022 17:53

--- controllers/mainController.php
+++ controllers/mainController.php
@@ -50,6 +50,32 @@ if (isset($_GET['chat'])) {
     die();
 }
 
+if (isset($_GET['direct_chat'])){
+        $db = Database::conexion();
+        $q = "SELECT chatid FROM user_chats WHERE chatid = (SELECT chatid FROM user_chats WHERE userid = ". $_SESSION['user']->getId() .") AND userid = " . $_GET['direct_chat'];
+        $result = $db->query($q);
+
+        if($result->num_rows){
+            var_dump($result->fetch_assoc());
+        }else{
+            $name = '(DIRECT) '. UserRepository::getUserById($_GET['direct_chat'])->getFirstname() . ' - '. $_SESSION['user']->getFirstName();
+            $db->query('INSERT INTO chats(id,name) VALUES (NULL,"'. $name .'")');
+            die();
+
+            //todo HACER INSER EN user_chats para la asociacion
+//            $q = "INSERT INTO user_chats(id,chatid,userid) VALUES (NULL,$_GET['])"
+        }
+
+}
+
+
+
+
+
+
+
+
+
 if(isset($_GET['id']) && !empty($_GET['id'])){
     $repository = new MessageRepository();
     $messages = $repository->getMessages($_GET['id']);
--- whatsapp.sql
+++ whatsapp.sql
@@ -11,62 +11,70 @@
  Target Server Version : 100422
  File Encoding         : 65001
 
- Date: 23/11/2022 21:35:46
+ Date: 24/11/2022 18:01:12
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for chat
+-- Table structure for chats
 -- ----------------------------
-DROP TABLE IF EXISTS `chat`;
-CREATE TABLE `chat`  (
+DROP TABLE IF EXISTS `chats`;
+CREATE TABLE `chats`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of chats
+-- ----------------------------
+INSERT INTO `chats` VALUES (1, 'GRUPO 1');
+INSERT INTO `chats` VALUES (4, 'Grupo 2');
+
+-- ----------------------------
+-- Table structure for messages
+-- ----------------------------
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `fecha` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `userid` int NOT NULL,
-  `groupid` int NOT NULL,
+  `chatid` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 42 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Records of chat
+-- Records of messages
 -- ----------------------------
-INSERT INTO `chat` VALUES (9, 'Ahora si wey', '21/11/22', 4, 0);
-INSERT INTO `chat` VALUES (10, 'hola que ase\r\n', '21/11/22', 1, 0);
-INSERT INTO `chat` VALUES (13, '¿Como estas Juan?', '21/11/22', 4, 0);
-INSERT INTO `chat` VALUES (14, 'PARALAAAA WECHO WACHOOOO\r\n', '21/11/22', 1, 0);
-INSERT INTO `chat` VALUES (15, 'holaa', '23/11/22', 4, 0);
+INSERT INTO `messages` VALUES (33, 'fdgdfg', '24/11/22', 4, 1);
+INSERT INTO `messages` VALUES (34, 'Hola', '24/11/22', 1, 1);
+INSERT INTO `messages` VALUES (35, 'Hola', '24/11/22', 1, 1);
+INSERT INTO `messages` VALUES (36, 'Hola', '24/11/22', 1, 1);
+INSERT INTO `messages` VALUES (37, 'dsfasa', '24/11/22', 1, 1);
+INSERT INTO `messages` VALUES (38, 'Hola', '24/11/22', 1, 4);
+INSERT INTO `messages` VALUES (39, 'Hola', '24/11/22', 1, 4);
+INSERT INTO `messages` VALUES (40, 'prueba', '24/11/22', 4, 1);
+INSERT INTO `messages` VALUES (41, 'gfdgf', '24/11/22', 1, 1);
 
 -- ----------------------------
--- Table structure for groups
+-- Table structure for user_chats
 -- ----------------------------
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE `groups`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of groups
--- ----------------------------
-
--- ----------------------------
--- Table structure for user_groups
--- ----------------------------
-DROP TABLE IF EXISTS `user_groups`;
-CREATE TABLE `user_groups`  (
+DROP TABLE IF EXISTS `user_chats`;
+CREATE TABLE `user_chats`  (
   `id` int NOT NULL,
   `userid` int NOT NULL,
-  `groupid` int NOT NULL,
+  `chatid` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user_groups
+-- Records of user_chats
 -- ----------------------------
+INSERT INTO `user_chats` VALUES (0, 1, 1);
+INSERT INTO `user_chats` VALUES (2, 2, 1);
 
 -- ----------------------------
 -- Table structure for users
@@ -88,8 +96,8 @@ CREATE TABLE `users`  (
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (1, 'Chema', 'Chema', 'Sanchez', 'c4ca4238a0b923820dcc509a6f75849b', 0, 'default.png', 'offline', 1669234952);
-INSERT INTO `users` VALUES (4, 'admin', 'Juan', 'Jimenez', '21232f297a57a5a743894a0e4a801fc3', 0, 'admin.png', 'offline', 1669232367);
+INSERT INTO `users` VALUES (1, 'Chema', 'Chema', 'Sanchez', 'c4ca4238a0b923820dcc509a6f75849b', 0, 'default.png', 'offline', 1669307522);
+INSERT INTO `users` VALUES (4, 'admin', 'Juan', 'Jimenez', '21232f297a57a5a743894a0e4a801fc3', 0, 'admin.png', 'offline', 1669309204);
 INSERT INTO `users` VALUES (6, 'Prueba', 'Prueba', 'Jimemez Sanchez', 'c893bad68927b457dbed39460e6afd62', 0, 'default.png', 'offline', 1669233069);
 
 SET FOREIGN_KEY_CHECKS = 1;
